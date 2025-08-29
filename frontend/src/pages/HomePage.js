9f23bf5aa1ce075b919ea39ee8225055f4cecb39
yogeshdevnani <yogeshdevnani@hotmail.com>
2025-08-29 13:30:45 -0300

    wake up backend when homepage loads

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import Divider from "@mui/material/Divider";
 import Category from "../components/homePage/Category";
 import ProductsPage from "../components/homePage/ProductsPage";
@@ -8,6 +8,13 @@ import ErrorPage from "../components/ErrorPage";
 
 //this component will render whole homepage along with header, products, etc.
 const HomePage = () => {
+  useEffect(() => {
+    // Wake up backend silently using test endpoint
+    fetch(process.env.REACT_APP_BACKEND_SERVER + "/test/testroute").catch(
+      () => {}
+    ); // Silent - don't show errors to user
+  }, []);
+
   return (
     <Box>
       {localStorage.getItem("Token") ? (
